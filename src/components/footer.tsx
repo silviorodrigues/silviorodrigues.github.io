65f6f40ef30f08e6a90dbd5e25659216b90e1ae4
Silvio Rodrigues <silvio@koho.ca>
2023-11-15 19:48:09 -0500

    Footer links

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,16 +2,16 @@ export default function Footer() {
   return (
     <footer className="p-4 flex items-center justify-center">
       <nav className="flex gap-12">
-        <a href="#" className="p-4 font-semibold text-sm text-neutral-400">
+        <a href="mailto:hi@silviorodrigues.dev" className="p-4 font-semibold text-sm text-neutral-400">
           email
         </a>
-        <a href="#" className="p-4 font-semibold text-sm text-neutral-400">
+        <a href="https://www.linkedin.com/in/silviorrr/" target="_blank" className="p-4 font-semibold text-sm text-neutral-400">
           linkedin
         </a>
-        <a href="#" className="p-4 font-semibold text-sm text-neutral-400">
+        <a href="https://twitter.com/silviorodrigu3s" target="_blank" className="p-4 font-semibold text-sm text-neutral-400">
           twitter
         </a>
-        <a href="#" className="p-4 font-semibold text-sm text-neutral-400">
+        <a href="https://github.com/silviorodrigues" target="_blank" className="p-4 font-semibold text-sm text-neutral-400">
           github
         </a>
       </nav>
