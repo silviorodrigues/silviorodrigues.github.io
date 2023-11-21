1e468b007333b0d47f900a905a41e6fd74347a63
Silvio Rodrigues <silvio@koho.ca>
2023-11-20 19:47:05 -0500

    Home content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <div className="flex flex-col justify-center">
-      <p className="font-bold text-7xl">
-        Hey 👋
+      <p className="font-bold text-4xl mb-8">
+        Silvio Rodrigues
+      </p>
+      <p className="font-bold mb-2">
+        Senior Frontend Developer 💅
+      </p>
+      <p className="text-neutral-400 mb-2">
+        currently working at <a className="underline underline-offset-4" href="https://www.koho.ca/" target="_blank">KOHO</a> 💼
+      </p>
+      <p className="text-neutral-400">
+        beginner maker, check <Link className="underline underline-offset-4" href="/projects">my projects</Link> 🧪
       </p>
     </div>
   )
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,7 +8,7 @@ export default function Header() {
   const activeLink = (url: string) => pathName === url ? 'nav-active' : '';
 
   return (
-    <header className="sm:p-4 sm:flex-row flex-col flex items-center justify-between">
+    <header className="px-4 py-2 flex-row flex items-center sm:justify-between justify-around">
       <Link href="/" className="font-bold p-4">
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -16,10 +16,9 @@ export default function Header() {
           fill="currentColor"
           viewBox="0 0 24 24"
         >
-          <path d="M12.963 2.286a.75.75 0 00-1.071-.136 9.742 9.742 0 00-3.539 6.177A7.547 7.547 0 016.648 6.61a.75.75 0 00-1.152-.082A9 9 0 1015.68 4.534a7.46 7.46 0 01-2.717-2.248zM15.75 14.25a3.75 3.75 0 11-7.313-1.172c.628.465 1.35.81 2.133 1a5.99 5.99 0 011.925-3.545 3.75 3.75 0 013.255 3.717z" />
+          <path d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z" />
+          <path d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z" />
         </svg>
-
-        Silvio Rodrigues
       </Link>
 
       <nav className="flex items-center sm:gap-12">
