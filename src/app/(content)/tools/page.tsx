c1e99314b70386f81f97d11638ecab5113dfb86b
Silvio Rodrigues <silvio@koho.ca>
2023-12-29 19:09:02 -0500

    Tools list

--- src/app/(content)/tools/page.tsx
+++ src/app/(content)/tools/page.tsx
@@ -1,4 +1,5 @@
 import PageTitle from '@/components/page-title';
+import tools from '@/data/tools';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -6,9 +7,40 @@ export const metadata: Metadata = {
 }
 
 export default function Tools() {
+  const renderTools = () =>
+    Object.entries(tools).map(([_, category], index) => {
+      return (
+        <div className="mb-16" key={index}>
+          <h2 className="font-bold text-2xl mb-2">{ category.name }</h2>
+          <ul className="list-disc list-outside ml-8">
+            { category.items.map((tool, index) => {
+              return (
+                <li className="text-neutral-400 text-[.92em]" key={index}>
+                  <a href={ `${tool.url}?utm_source=silviorodrigues.dev` } target="_blank" className="underline underline-offset-4 text-neutral-200">
+                    { tool.name }
+                  </a> - { tool.description } {
+                    tool.code && tool.bonus && renderCode(tool.code, tool.bonus)
+                  }
+                </li>
+              )
+            }) }
+          </ul>
+        </div>
+      )
+    });
+
+  const renderCode = (code: string, bonus: string) => {
+    return (
+      <span className="text-neutral-200 italic">
+        • Get <span className="font-bold">{ bonus }</span> using my code <code className="p-1 rounded text-sm bg-green-900 text-green-200">{ code }</code>
+      </span>
+    )
+  }
+
   return (
     <>
       <PageTitle text="Tools I use" emoji="🛠️"/>
+      { renderTools() }
     </>
   )
 }
---
+++ src/data/tools.ts
@@ -0,0 +1,143 @@
+enum ToolCategory {
+  Setup = 'setup',
+  Coding = 'coding',
+  Apps = 'apps',
+  Finances = 'finances'
+}
+
+type Tool = {
+  name: string;
+  description: string;
+  url: string;
+  code?: string;
+  bonus?: string;
+}
+
+type ToolCategoryType = {
+  name: string;
+  items: Tool[];
+}
+
+type ToolsType = {
+  [key in ToolCategory]: ToolCategoryType;
+}
+
+const tools: ToolsType = {
+  setup: {
+    name: 'Setup',
+    items: [
+      {
+        name: 'MacBook M1 Pro (16", 2021)',
+        description: 'Run IOS + Android emulators and open 100 Chrome tabs at the same time? Easy!',
+        url: 'https://amzn.to/47h8Ej9'
+      },
+      {
+        name: 'Samsung 32" 4K Curved Monitor',
+        description: 'A lot of space to open many windows, a lot...',
+        url: 'https://amzn.to/3tE7lNB'
+      },
+      {
+        name: 'NuPhy Air60 Keyboard',
+        description: 'Fantastic design and ideal size for working from home or on the road',
+        url: 'https://nuphy.com/products/air60?sca_ref=5139327.wsXOC5wpXj'
+      },
+      {
+        name: 'Sony WH-1000XM5 Headphones',
+        description: 'The best noise cancellation and sound quality I\'ve ever had',
+        url: 'https://amzn.to/3TIWhcH'
+      },
+      {
+        name: 'Razer Kiyo Pro Webcam',
+        description: 'I only use it for meetings, but if I were a streammer I would already be covered',
+        url: 'https://amzn.to/3TBaJ6y'
+      },
+      {
+        name: 'VictoryLX Adjustable Desk',
+        description: 'Working standing helps me focus and is good for the back',
+        url: 'https://www.esiergo.com/height-adjustable-tables/electric/complete-tables/victory-2-leg/sp/victorylx-2-leg-tfl/'
+      }
+    ]
+  },
+  coding: {
+    name: 'Coding',
+    items: [
+      {
+        name: 'VSCode',
+        description: 'It took me time to migrate after years with Sublime (and a shot with Vim)',
+        url: 'https://code.visualstudio.com/'
+      },
+      {
+        name: 'Dracula Theme',
+        description: 'Dark mode is love! My official theme since 2015',
+        url: 'https://draculatheme.com/visual-studio-code/'
+      },
+      {
+        name: 'Oh My ZSH',
+        description: 'Handy collection of ZSH plugins',
+        url: 'https://ohmyz.sh/'
+      }
+    ]
+  },
+  apps: {
+    name: 'Apps',
+    items: [
+      {
+        name: 'Notion',
+        description: 'My second brain. Ideas, to-do lists, notes, everything is concentrated here',
+        url: 'https://www.notion.so/'
+      },
+      {
+        name: '1Password',
+        description: 'Safety first, a must these days',
+        url: 'https://1password.com/'
+      },
+      {
+        name: 'Todoist',
+        description: 'When I need quick and easy to manage lists, like groceries',
+        url: 'https://todoist.com/'
+      },
+      {
+        name: 'Divvy',
+        description: 'To organize all open windows on a large monitor',
+        url: 'https://mizage.com/divvy/'
+      },
+      {
+        name: 'Grammarly',
+        description: 'Perfect for correcting everything from small typos to looong messages',
+        url: 'https://grammarly.com/'
+      }
+    ]
+  },
+  finances: {
+    name: 'Finances',
+    items: [
+      {
+        name: 'Organizze',
+        description: 'Where I organize all my finances',
+        url: 'https://www.organizze.com.br/'
+      },
+      {
+        name: 'KOHO',
+        description: 'Financial life becomes much easier with KOHO',
+        url: 'https://web.koho.ca/referral/SILVIOKOHO',
+        code: 'SILVIOKOHO',
+        bonus: '$20'
+      },
+      {
+        name: 'Wealthsimple',
+        description: 'A very intuitive and complete investment platform',
+        url: 'https://wealthsimple.com/',
+        code: '6CB9OG',
+        bonus: '$25'
+      },
+      {
+        name: 'Nubank',
+        description: 'Brazilian neo-bank that I have been using it since its launch',
+        url: 'https://nubank.com.br/en/'
+      }
+    ]
+
+  }
+}
+
+export default tools;
